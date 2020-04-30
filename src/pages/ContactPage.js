e2ffb975d740d90f55951ea68c85890a480dbca7
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-04-30 19:31:06 +0200

    contact and about page added

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 import { motion } from "framer-motion";
 import { pageVariants, pageTransition } from "../components/pageTransition";
+import "../styles/ContactPage.scss";
 
 class ContactPage extends Component {
-  state = {};
+  state = {
+    username: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDedault();
+  };
+
+  handleChange = (e) => {
+    const name = e.target.name;
+
+    this.setState({
+      [name]: e.target.value,
+    });
+  };
+
   render() {
     return (
       <motion.div
@@ -15,7 +34,60 @@ class ContactPage extends Component {
         transition={pageTransition}
       >
         <h2 className="header-big">contact with us</h2>
-        <form></form>
+        <div className="form_container">
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="input">
+              <label htmlFor="user">
+                <input
+                  type="text"
+                  id="user"
+                  name="username"
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  placeholder="Name"
+                />
+              </label>
+            </div>
+            <div className="input">
+              <label htmlFor="email">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  placeholder="Email"
+                />
+              </label>
+            </div>
+            <div className="input">
+              <label htmlFor="phone">
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={this.state.phone}
+                  onChange={this.handleChange}
+                  placeholder="Phone"
+                />
+              </label>
+            </div>
+            <div className="input">
+              <label htmlFor="message">
+                <textarea
+                  name="message"
+                  id="message"
+                  value={this.state.message}
+                  onChange={this.handleChange}
+                  placeholder="Your message"
+                ></textarea>
+              </label>
+            </div>
+            <button type="button" className="send_btn">
+              Send
+            </button>
+          </form>
+        </div>
       </motion.div>
     );
   }
